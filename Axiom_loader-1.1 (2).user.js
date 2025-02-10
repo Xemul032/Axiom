20c751bdb9ac786a8e0386b40517494cf20230f9
Xemul032 <ritavsvitere@gmail.com>
2025-02-10 11:50:36 +0300

    Update Axiom_loader-1.1 (2).user.js

--- Axiom_loader-1.1 (2).user.js
+++ Axiom_loader-1.1 (2).user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name Axiom_loader
 // @namespace http://tampermonkey.net/
-// @version 1.1
-// @description Загружает и выполняет скрипт с GitHub с автоматическим обновлением
+// @version 1.0
+// @description Загружает и выполняет скрипт с GitHub
 // @author Ваше имя
 // @match https://cplink.simprint.pro/*
 // @icon https://cplink.simprint.pro/axiom/img/icon/icon32.png
@@ -10,52 +10,36 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+'use strict';
 
-    // URL основного скрипта на GitHub
-    const scriptUrl = 'https://raw.githubusercontent.com/Xemul032/Axiom/refs/heads/main/Axioma_beta.js';
+// URL основного скрипта на GitHub
+const scriptUrl = 'https://raw.githubusercontent.com/Xemul032/Axiom/refs/heads/main/Axioma_beta.js';
 
-    // Функция для загрузки и исполнения скрипта
-    function loadScript(url) {
-        GM_xmlhttpRequest({
-            method: 'GET',
-            url: url,
-            onload: function(response) {
-                // Проверяем, успешно ли загружен скрипт
-                if (response.status === 200) {
-                    try {
-                        // Очищаем предыдущий скрипт (если есть)
-                        if (window.axiomCleanup && typeof window.axiomCleanup === 'function') {
-                            window.axiomCleanup();
-                        }
-                        // Выполняем загруженный скрипт
-                        eval(response.responseText);
-                    } catch (e) {
-                        console.error('Ошибка при выполнении скрипта:', e);
-                    }
-                } else {
-                    console.error('Ошибка загрузки скрипта: HTTP статус', response.status);
-                }
-            },
-            onerror: function(err) {
-                console.error('Ошибка при запросе:', err);
-            }
-        });
-    }
+// Функция для загрузки и исполнения скрипта
+function loadScript(url) {
+GM_xmlhttpRequest({
+method: 'GET',
+url: url,
+onload: function(response) {
+// Проверяем, успешно ли загружен скрипт
+if (response.status === 200) {
+try {
+// Выполняем загруженный скрипт
+eval(response.responseText);
+} catch (e) {
+console.error('Ошибка при выполнении скрипта:', e);
+}
+} else {
+console.error('Ошибка загрузки скрипта: HTTP статус', response.status);
+}
+},
+onerror: function(err) {
+console.error('Ошибка при запросе:', err);
+}
+});
+}
 
-    // Функция для периодической перезагрузки скрипта
-    function startAutoReload() {
-        // Загружаем скрипт сразу при запуске
-        loadScript(scriptUrl);
-
-        // Устанавливаем интервал перезагрузки (30 минут = 1800000 миллисекунд)
-        setInterval(() => {
-            console.log('Перезагрузка скрипта...');
-            loadScript(scriptUrl);
-        }, 1000);
-    }
-
-    // Запускаем автоматическую перезагрузку
-    startAutoReload();
+// Загружаем и запускаем скрипт
+loadScript(scriptUrl);
 
 })();
