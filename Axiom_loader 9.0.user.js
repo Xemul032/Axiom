f13157d32880e0a7e9394ef24a862cb8835a8016
Xemul032 <ritavsvitere@gmail.com>
2025-03-18 13:27:20 +0300

    Update Axiom_loader 9.0.user.js

--- Axiom_loader 9.0.user.js
+++ Axiom_loader 9.0.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Axiom_loader 9.0
+// @name         Axiom_loader 9.1
 // @namespace    http://tampermonkey.net/
 // @version      1.1
 // @description  Загружает скрипт с GitHub, отправляет IP в Google таблицу и извлекает ProductId из кнопок
