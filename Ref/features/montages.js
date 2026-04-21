303a3cf6875db54c48b8ecee88f3a4b6f91b4847
Xemul032 <ritavsvitere@gmail.com>
2026-04-21 16:46:35 +0300

    Update montages.js

--- Ref/features/montages.js
+++ Ref/features/montages.js
@@ -26,7 +26,7 @@
     const UNIQUE_PREFIX = 'montage-script-v2-';
     let buttonAdded = false;
     let createdButton = null;
-    let productIdCache = new Set();
+    let productIdCache = new Map();
     let isProcessing = false;
     let typesCache = [];
     let files = [];
